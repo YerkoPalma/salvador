f34d823ab376f13b34cd77a754e340f1b33a73a4
YerkoPalma <yerko.palma@usach.cl>
2016-08-11 00:06:06 -0400

    choo init

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const choo = require('choo')
+const sf = require('sheetify')
+const mainView = require('./views/main')
+
+sf('./style.css', { global: true })
+
+const app = choo()
+
+app.model(require('./models/event'))
+
+app.router(route => [
+  route('/', mainView)
+])
+
+const tree = app.start()
+document.body.appendChild(tree)
