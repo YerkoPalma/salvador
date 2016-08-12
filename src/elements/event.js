6feb84b60b9445382d53d7de0a486c1b8d682c72
Yerko Palma <yerko.palma@usach.cl>
2016-08-12 15:51:16 +0000

    improved build process

--- src/elements/event.js
+++ src/elements/event.js
@@ -3,7 +3,7 @@ const html = require('choo/html')
 function timeEvent (event) {
   return html`
   <li class="event">
-    <input type="radio" name="tl-group" checked/>
+    <input type="radio" name="tl-group" checked />
     <label></label>
     <div class="thumb user-4"><span>${event.date}</span></div>
     <div class="content-perspective">
