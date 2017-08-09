71da72eb9339548e6b8db8b4532fe3f7c0caf603
Yerko Palma <yerko.palma@usach.cl>
2017-08-09 18:15:43 +0000

    minimal functional choo 6 code

---
+++ index.js
@@ -0,0 +1,30 @@
+var html = require('choo/html')
+var log = require('choo-log')
+var choo = require('choo')
+
+var app = choo()
+app.use(log())
+app.use(countStore)
+app.route('/', mainView)
+app.mount('body')
+
+function mainView (state, emit) {
+  return html`
+    <body>
+      <h1>count is ${state.count}</h1>
+      <button onclick=${onclick}>Increment</button>
+    </body>
+  `
+
+  function onclick () {
+    emit('increment', 1)
+  }
+}
+
+function countStore (state, emitter) {
+  state.count = 0
+  emitter.on('increment', function (count) {
+    state.count += count
+    emitter.emit('render')
+  })
+}
