c68884a26093c72feb02df31b57703c838893b0f
Yerko Palma <yerko.palma@usach.cl>
2017-08-25 13:49:14 +0000

    added numeral and travis update

--- index.js
+++ index.js
@@ -6,10 +6,11 @@ css('./style.css', { global: true })
 
 var app = choo()
 if (process.env.NODE_ENV !== 'production') {
-  app.use(require('choo-log')())
+  app.use(require('choo-log')({ timing: false }))
   app.use(require('choo-service-worker/clear')())
+} else {
+  app.use(require('choo-service-worker')())
 }
-app.use(require('choo-service-worker')())
 app.use(function (state, emitter) {
   var allYears = require('./data.json')
 
@@ -19,7 +20,12 @@ app.use(function (state, emitter) {
   state.availaibleYears = 2
   state._events = allYears[0]
   emitter.on('setYear', function (year) {
+    var numeral = 'primer'
+    numeral = year === 1
+              ? 'primer' : year === 2
+              ? 'segundo' : 'tercer'
     state.current = year
+    state.subtitle = 'Su ' + numeral + ' añito'
     state._events = allYears[year - 1]
     emitter.emit('log:info', 'Going to ' + year)
     emitter.emit(state.events.PUSHSTATE, '/?year=' + year)
