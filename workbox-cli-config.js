48af808ecec78a645cce8a50caa74d733b8ef447
Yerko Palma <yerko.palma@usach.cl>
2017-08-25 18:18:51 +0000

    updated strategies for sw

--- workbox-cli-config.js
+++ workbox-cli-config.js
@@ -1,10 +1,30 @@
 module.exports = {
   "globDirectory": "dist/",
+  "swDest": "dist/sw.js",
   "globPatterns": [
     "**/*.{eot,svg,ttf,woff,woff2,jpg,png,mp4,css,js,html}"
   ],
-  "swDest": "dist/sw.js",
   "globIgnores": [
     "../workbox-cli-config.js"
-  ]
+  ],
+  "runtimeCaching": [{
+    urlPattern: /.eot|svg|ttf|woff|woff2|png|jpg|mp4$/,
+    handler: 'cacheFirst',
+    options: {
+      cacheName: 'media-cache'
+    }
+  },{
+    urlPattern: /.js|html|css$/,
+    handler: 'staleWhileRevalidate',
+    options: {
+      cacheName: 'data-cache'
+    }
+  },{
+    urlPattern: /\.(?!(eot|svg|ttf|woff|woff2|png|jpg|mp4|js|html|css)).*$/,
+    handler: 'networkFirst',
+    options: {
+      cacheName: 'other-cache',
+      networkTimeoutSeconds: 3
+    }
+  }]
 };
